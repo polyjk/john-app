52784f767a337e9a6ee979262aa970775a3b0242
John <p5khamm@gmail.comm>
2021-05-12 17:01:06 -0500

    added submit form

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import Note from "./Note";
 
 const App = (props) => {
   console.log(props);
-  const { notes } = props;
+  // const { notes } = props;
+  const [notes, setNotes] = useState(props.notes);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log("ButtonClicked", event.target);
+  };
 
   return (
     <div className="App">
       <header className="App-header">Header</header>
       <ul>
         {notes.map((note) => (
-          <Note note={note}></Note>
+          <Note key={note.id} note={note}></Note>
         ))}
       </ul>
+
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
